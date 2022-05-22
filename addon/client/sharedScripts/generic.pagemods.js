52fc785e0176d331f13a1c283faabcb8c0f5caf9
aliko <a.minyukovich@gmail.com>
2022-05-22 04:08:24 +0200

    New generic pagemods

--- addon/client/sharedScripts/generic.pagemods.js
+++ addon/client/sharedScripts/generic.pagemods.js
@@ -2,29 +2,48 @@
 // scripts to be applied to each webpage - to remove pop-ups, top/bottom bars etc.
 
 (()=>{
+	const noActionExceptions = ["crimsouneclub.com", "fabindia.com", "corneliajames.com", "saintandsofia.com"]; // because some of the genericPageMods cause page re-load
+	
 	function __ssGenericPageMod(){
+		if(noActionExceptions.some(x=>window.location.href.indexOf(x) > -1)){
+			console.log("SKIPPING Rolling generic page mods");
+			return; // no action exception
+		}
 		console.log("Rolling generic page mods");
 		// removing FF out of date top bars
 		document.querySelectorAll("#buorg.buorg").forEach(el => {
-			console.log("[pageMod] Generic element removed.", location.href);
+			console.log("[pageMod] Generic FF-outdated element removed.", location.href);
 			el.remove();
+			document.documentElement.style["margin-top"] = "initial";
 		});
 		// google translate banners
 		document.querySelectorAll(".goog-te-banner-frame.skiptranslate").forEach(el => {
 			console.log("[PageMode] Removng Google translate iframe", location.href);
 			el.remove();
 		});
+		// Common alert banner x buttons
+		const bannerCloseButtons = [".klaviyo-close-form", ".js-usp-close", ".modal-close", ".js-modal-close", ".fancybox-close-small", "[data-wps-popup-close]", ".wisepops-close", ".s-close-popup", ".close-modal", ".ui-dialog-titlebar-close"];
+		// Common cookieButtons to be clicked
+		const cookieButtons2Click = ["#CybotCookiebotDialogBodyButtonDecline", "button.close", ".recommendation-modal__close-button", "#btn-cookie-allow", ".js-cookie-consent-close", ".cookie-banner-accept", ".cookie-accept-button", ".js-accept-gdpr", ".eg-cc-dismiss", "#inputAcceptCookies", ".cookie-notice__close"].concat(bannerCloseButtons).join(",");
+		document.querySelectorAll(cookieButtons2Click).forEach(x=>x.click());
 		// Common cookie-notice providers/messages
-		const commonCookieContainerSelectors = ["#onetrust-banner-sdk", "#onetrust-consent-sdk", ".cc-window.cc-banner.cc-bottom", "#cookie-bar, #cookie-law-info-bar, #cookie-notice", ".cookie-policy.cookie-policy--open", "#__tealiumGDPRecModal"].join(",");
+		const commonCookieContainerSelectors = ["#onetrust-banner-sdk", "#onetrust-consent-sdk", ".cc-window.cc-banner.cc-bottom", "#cookie-bar, #cookie-law-info-bar, #cookie-notice", ".cookie-policy.cookie-policy--open", "#__tealiumGDPRecModal", "#cookie_alert"].join(",");
 		document.querySelectorAll(commonCookieContainerSelectors).forEach(el=>el.remove());
 		// Generic Overlays
-		const commonOverlaySelectors = [".pum-overlay", ".overlay_11", "#boxpopup0", "#boxpopup1", "#boxpopup2", "#boxpopup3", "#boxpopup", ".md-overlay", "#myModal", ".modal-backdrop"];
+		const commonOverlaySelectors = [".pum-overlay", ".overlay_11", "#boxpopup0", "#boxpopup1", "#boxpopup2", "#boxpopup3", "#boxpopup", ".md-overlay", "#myModal", ".modal-backdrop", "#shopify-section-popup", '#onesignal-slidedown-container'];
 		document.querySelectorAll(commonOverlaySelectors).forEach(el=>el.remove());
 		const commonNoScrollClasses = ["pum-open-overlay", "modal-open"];
 		commonNoScrollClasses.forEach(c => {
 			document.documentElement.classList.remove(c);
 			document.body.classList.remove(c);
 		});
+		// Overlays with style-fixed overflows on body/html
+		const fixedBodySels = ["#attentive_overlay"].join(",");
+		const fixedBodyEls = document.querySelectorAll(fixedBodySels);
+		if(fixedBodyEls){
+			fixedBodyEls.forEach(x => x.remove());
+			document.documentElement.style = document.body.style = null;
+		}
 		// Removing scroll-dependent hiding methods -- For common Libs
 		const aosSliders = document.querySelectorAll("[data-aos]");
 		if(aosSliders.length){
